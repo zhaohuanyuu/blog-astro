6b01030d9b588ef57e9a51e188ddece5903d2c73
zhaohuanyu <zhaohuanyu@aliyun.com>
2023-11-13 22:29:09 +0800

    add biome linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx"
+import solidJs from "@astrojs/solid-js"
+import { defineConfig } from 'astro/config'
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), mdx()]
+  integrations: [
+    mdx(),
+    solidJs(),
+    vanillaExtractPlugin()
+  ]
 });
