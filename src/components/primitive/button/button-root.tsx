0fde87f8bd30f5850b0bb1274791d323927a5150
zhaohuanyu <zhaohuanyu@aliyun.com>
2024-03-04 20:56:47 +0800

    feat: primitive component in progress

--- src/components/primitive/button/button-root.tsx
+++ src/components/primitive/button/button-root.tsx
@@ -1,8 +1,68 @@
 import { createMemo, splitProps } from "solid-js"
 import {
-  type mergeDefaultProps,
+  mergeRefs,
+  mergeDefaultProps,
   type OverrideComponentProps,
-  mergeRefs
 } from "../../../common/utils"
+import { isButton } from "./is-button"
+import { createTagName } from "../creations"
 import { type AsChildProp, Polymorphic } from "../polymorphic";
 
+export interface ButtonRootOptions extends AsChildProp {
+	disabled?: boolean;
+}
+
+export interface ButtonRootProps extends OverrideComponentProps<"button", ButtonRootOptions> {}
+
+export function ButtonRoot(props: ButtonRootProps) {
+	let ref: HTMLButtonElement | undefined;
+
+	const mergedProps = mergeDefaultProps({ type: "button" }, props);
+
+	const [local, others] = splitProps(mergedProps, ["ref", "type", "disabled"]);
+
+	const tagName = createTagName(
+		() => ref,
+		() => "button",
+	);
+
+	const isNativeButton = createMemo(() => {
+		const elementTagName = tagName();
+
+		if (elementTagName == null) {
+			return false;
+		}
+
+		return isButton({ tagName: elementTagName, type: local.type });
+	});
+
+	const isNativeInput = createMemo(() => {
+		return tagName() === "input";
+	});
+
+	const isNativeLink = createMemo(() => {
+		return tagName() === "a" && ref?.getAttribute("href") != null;
+	});
+
+	return (
+		<Polymorphic
+			as="button"
+			ref={mergeRefs((el) => (ref = el), local.ref)}
+			type={isNativeButton() || isNativeInput() ? local.type : undefined}
+			role={!isNativeButton() && !isNativeLink() ? "button" : undefined}
+			tabIndex={
+				!isNativeButton() && !isNativeLink() && !local.disabled ? 0 : undefined
+			}
+			disabled={
+				isNativeButton() || isNativeInput() ? local.disabled : undefined
+			}
+			aria-disabled={
+				!isNativeButton() && !isNativeInput() && local.disabled
+					? true
+					: undefined
+			}
+			data-disabled={local.disabled ? "" : undefined}
+			{...others}
+		/>
+	);
+}
---
+++ src/components/primitive/creations/create-tag-name.ts
@@ -0,0 +1,18 @@
+import { isString } from "../../../common/utils"
+import { createSignal, createEffect, type Accessor, type Component } from "solid-js"
+
+
+export function createTagName(
+  ref: Accessor<HTMLElement | undefined>,
+  fallback?: Accessor<string | Component | undefined>
+) {
+  const [tagName, setTagName] = createSignal(stringOrUndefined(fallback?.()));
+  createEffect(() => {
+		setTagName(ref()?.tagName.toLowerCase() || stringOrUndefined(fallback?.()));
+	})
+  return tagName
+}
+
+function stringOrUndefined(value: any) {
+	return isString(value) ? value : undefined;
+}
