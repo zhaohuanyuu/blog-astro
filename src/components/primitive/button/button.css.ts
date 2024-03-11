60cff695c31a8ac4bcf2fed02d0b1eb8b401774f
zhaohuanyu <zhaohuanyu@aliyun.com>
2024-03-11 23:00:46 +0800

    feat: add button variants

--- src/components/primitive/button/button.css.ts
+++ src/components/primitive/button/button.css.ts
@@ -1,7 +1,7 @@
-import { style } from "@vanilla-extract/css"
-import { vars } from "@styles/theme/contract.css"
-import { transition } from "@styles/tokens/motion"
-import { pseudoSelectors } from '@styles/utils/selectors'
+import { style, styleVariants } from "@vanilla-extract/css"
+import { vars } from "../../../styles/theme/contract.css"
+import { transition } from "../../../styles/tokens/motion"
+import { pseudoSelectors } from '../../../styles/utils/selectors'
 
 export const buttonBaseStyle = style({
   position: 'relative',
@@ -26,6 +26,21 @@ export const buttonBaseStyle = style({
       opacity: .4,
       boxShadow: 'none',
       cursor: 'not-allowed'
+    }
+  }
+})
+
+export const buttonVariants = styleVariants({
+  ghost: {
+    color: 'inherit',
+    backgroundColor: 'transparent',
+    selectors: {
+      [pseudoSelectors.hover]: {
+        backgroundColor: vars.color.ghostBg
+      },
+      [pseudoSelectors.active]: {
+        backgroundColor: vars.color.ghostBg
+      }
     }
   }
 })
