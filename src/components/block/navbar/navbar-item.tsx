193a100e6f4654c2a700e8f058c0c8d4be88d467
zhaohuanyu <zhaohuanyu@aliyun.com>
2024-04-26 23:24:07 +0800

    feat: component creation migrate to use hooks

--- src/components/block/navbar/navbar-item.tsx
+++ src/components/block/navbar/navbar-item.tsx
@@ -1,7 +1,12 @@
 import { Polymorphic } from "@components/primitive/polymorphic"
 
 const NavbarItem = () => {
-  return <Polymorphic as="li" />
+  return (
+    <Polymorphic
+      as="li"
+      role="navbar-item"
+    />
+  )
 }
 
 export default NavbarItem
