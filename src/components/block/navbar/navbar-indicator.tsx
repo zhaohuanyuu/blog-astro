f6c4861d9e3363a6ef0394b12408123830328ecd
zhaohuanyu <zhaohuanyu@aliyun.com>
2024-04-28 00:08:46 +0800

    feat: navbar context logic

--- src/components/block/navbar/navbar-indicator.tsx
+++ src/components/block/navbar/navbar-indicator.tsx
@@ -2,7 +2,10 @@ import { Polymorphic } from "@components/primitive/polymorphic"
 
 const NavbarIndicator = (props) => {
   return (
-    <Polymorphic as="div" role="presentation" />
+    <Polymorphic
+      as="div"
+      role="presentation"
+    />
   )
 }
 
